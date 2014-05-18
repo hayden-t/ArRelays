804c59acd2892852039eb915f76f264f32d10c50
hayden-t <hayden@noreply.com>
2014-05-18 17:24:34 +1000

    lots of changes

--- ArRelays.ino
+++ ArRelays.ino
@@ -1,47 +1,49 @@
-//#include <Wire.h>
+#include <Wire.h>
 #include <LiquidCrystal.h>
-//#include <EEPROM.h>
-
+#include <EEPROM.h>
+#include <Button.h>
 
 typedef struct {
   char name[15];//max characters for name
   boolean toggle;//if false, = momentary
   boolean status;//on or off
   boolean saved;//if status saved to eeprom
-  boolean hibeam;//dependant on high beam active
+  boolean highbeam;//dependant on high beam active
+  int pin;//which pin number relay is connected to
 } relay;
 
 const int maxRelays = 13;//must equal number of specified relays
 
 relay relays[maxRelays + 1] = {
   {""},//not used
-  {"ROOF LIGHT BAR", true, false, true, true},
-  {"BULL BAR LIGHT", true, false, true, true},
-  {"LED SPOTTIES", true, false, true, true},
-  {"9\" HID LIGHT", true, false, true, true},
-  {"7\" HID LIGHT", true, false, true, true},
-  {"LEFT LED BAR", true, false, true, false},
-  {"RIGHT LED Bar", true, false, true, false},
-  {"REAR LED BAR", true, false, true, false},
-  {"AUX BATTERY", true, false, false, false},
-  {"WINCH MASTER", true, false, false, false},
-  {"WINCH IN", false, false, false, false},
-  {"WINCH OUT", false, false, false, false},
-  {"REVERSE CAMERA", true, false, false, false}      
+  {"ROOF LIGHT BAR", true, false, true, true, 23},
+  {"BULL BAR LIGHT", true, false, true, true, 25},
+  {"LED SPOTTIES", true, false, true, true, 27},
+  {"9\" HID LIGHT", true, false, true, true, 29},
+  {"7\" HID LIGHT", true, false, true, true, 31},
+  {"LEFT LED BAR", true, false, true, false, 33},
+  {"RIGHT LED Bar", true, false, true, false, 35},
+  {"REAR LED BAR", true, false, true, false, 37},
+  {"AUX BATTERY", true, false, false, false, 39},
+  {"WINCH MASTER", true, false, false, false, 41},
+  {"WINCH IN", false, false, false, false, 43},
+  {"WINCH OUT", false, false, false, false, 45},
+  {"REVERSE CAMERA", true, false, false, false, 47}      
 };
 
+int highBeamPin = 53;//input pin detect high beam status
+boolean highBeam = false;//high beam status
 
 //init LCD
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 int lcdLines = 4;
 
-int menuState = 0;
+int menuState = 1;
 
-int stateA0 = HIGH;
-int stateA1 = HIGH;
-int stateA2 = HIGH;
-int stateA3 = HIGH;
-unsigned long buttonTimer = 0;
+Button down = Button(A0, BUTTON_PULLUP_INTERNAL);
+Button up = Button(A1, BUTTON_PULLUP_INTERNAL);
+Button on = Button(A2, BUTTON_PULLUP_INTERNAL);
+Button off = Button(A3, BUTTON_PULLUP_INTERNAL);
 
 void setup() {
   // put your setup code here, to run once:
@@ -52,22 +54,21 @@ void setup() {
   //Wire.begin();
   lcd.begin(20, lcdLines);
   
-  pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
-  pinMode(A2, INPUT);
-  pinMode(A3, INPUT);
-  digitalWrite(A0, stateA0);
-  digitalWrite(A1, stateA1);
-  digitalWrite(A2, stateA2);
-  digitalWrite(A3, stateA3);
+   for(int i = 1; i <= maxRelays; i++){
+       pinMode(relays[i].pin, OUTPUT);
+  }
   
+  pinMode(highBeamPin, INPUT);   
+  
+  loadSettings();
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
     
-    checkButtons();
+    checkButtons();   
+    outputRelays();
     drawMenu();
 
 }
@@ -88,73 +89,67 @@ void drawMenu(void){
           lcd.setCursor(17, i);
           if(relays[menuState + i].status)lcd.print("ON ");
           else lcd.print("OFF");          
-
-
           
       }else if(!menuState)lcd.print("Back");
       
    } 
 }
 void checkButtons(void){
- //check buttons
  
 //down button 
-  int reading = analogRead(A0);
-  if((reading < 512) && (stateA0 == HIGH)){
-    stateA0 = LOW;   
-    //if(!openMenu){
-      //if(viewEvent < eventCount())viewEvent++;
-    //}else{
-      if(menuState < maxRelays)menuState++;
-    //}   
+
+ if(down.uniquePress()){
+    if(menuState < maxRelays)menuState++;    
     lcd.clear();
-    buttonTimer = millis();
-    //digitalWrite(ledPin, LOW);
-    //Serial.println("Button1");
-  }else if((reading > 512) && (stateA0 == LOW)){
-    stateA0 = HIGH;
   }
   
 //up button  
-  reading = analogRead(A1);
-  if((reading < 512) && (stateA1 == HIGH)){
-    stateA1 = LOW;    
-   // if(!openMenu){
-   //   if(viewEvent > 1)viewEvent--;
-   // }else{
-      if(menuState > 0)menuState--;
-    //}    
+  
+ if(up.uniquePress()){
+      if(menuState > 1)menuState--;
       lcd.clear();
-      buttonTimer = millis();
-      //digitalWrite(ledPin, LOW);
-    //Serial.println("Button2");
-  }else if((reading > 512) && (stateA1 == LOW)){
-    stateA1 = HIGH;
-  }
+   }
   
-//enter button 
-  reading = analogRead(A2);
-  if((reading < 512) && (stateA2 == HIGH)){
-    stateA2 = LOW;   
-    //if(!openMenu)openMenu = true;
-    //else toggleAlarm(menuState);    
-    lcd.clear();
-    buttonTimer = millis();
-    //digitalWrite(ledPin, LOW);
-    //Serial.println("Button3");
-  }else if((reading > 512) && (stateA2 == LOW)){
-    stateA2 = HIGH;
-  }
-  
-  //mode button 
-  reading = analogRead(A3);
-  if((reading < 512) && (stateA3 == HIGH)){
-    stateA3 = LOW;   
-   // changeMode();
-    buttonTimer = millis();
-  }else if((reading > 512) && (stateA3 == LOW)){
-    stateA3 = HIGH;
-  }
+//on/enter button 
+
+  if(relays[menuState].toggle){
+     if(on.uniquePress()){
+      relays[menuState].status = true;
+      lcd.clear();
+      if(relays[menuState].saved)EEPROM.write(menuState, 1);
+     }
+  }else{
+      if(on.isPressed())relays[menuState].status = true;
+      else relays[menuState].status = false;
+   }
+   
+  //off button 
+   if(off.uniquePress()){
+       relays[menuState].status = false;
+       if(relays[menuState].saved)EEPROM.write(menuState, 0);
+    }
   
   //if(((millis() - buttonTimer) > 10000) && (viewEvent != 1 || openMenu))home();
+}
+
+void outputRelays(void){
+  
+    highBeam = (analogRead(highBeamPin) > 205 ? true : false);//appx. 1 volt of more
+  
+     for(int i = 1; i <= maxRelays; i++){
+       if(relays[i].highbeam){//is subject to high beam
+         if(highBeam)digitalWrite(relays[i].pin, relays[i].status);//high beam on
+         else digitalWrite(relays[i].pin, false);//high beam off
+       }
+       else digitalWrite(relays[i].pin, relays[i].status);//not subject to high beam
+    }  
+    
+}
+
+void loadSettings(void){
+  
+  for(int i = 1; i <= maxRelays; i++){       
+      if(relays[i].saved)relays[i].status = (EEPROM.read(i) == 1 ? true : false);
+  }
+
 }
